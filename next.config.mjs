e8fe8c77e722dfc7eac887f168bdf18faa4d4a95
goodchuck <y502100@naver.com>
2024-04-18 19:47:20 +0900

    add : 이미지 및 폰트파일 추가 및 도메인적용

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['noticon-static.tammolo.com', 'loremflickr.com']
+    }
+};
 
 export default nextConfig;
